0fa90c046b92f0b7a3d780a9d3f5549c7eb09d30
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2024-09-14 02:14:34 +0100

    chore: exported app for tests

--- server/api.ts
+++ server/api.ts
@@ -4,5 +4,5 @@ import { config } from './config';
 import { ExpressApp } from './loaders/express';
 import { serve } from './serve';
 
-const app = new ExpressApp(express());
+export const app = new ExpressApp(express());
 serve(app, config.port);
